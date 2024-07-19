326a9dd0587fde150f2486f69d0ff259c9144a00
mina faysal <minafaysal.94@gmail.com>
2024-07-19 13:26:15 +0300

    implementation for not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
 export const routes: Routes = [
   {
     path: '',
     loadChildren: () =>
       import('./modules/auth/auth.module').then((m) => m.AuthModule),
-  }
+  },
+  { path: '**', component: NotFoundComponent },
 ];
