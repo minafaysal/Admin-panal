aa7af4174482ac599490f2caa4480e7dfe06f415
mina faysal <minafaysal.94@gmail.com>
2024-07-20 20:36:57 +0300

    implementation for edit category

--- src/app/modules/categories/categories-list/categories-list.component.ts
+++ src/app/modules/categories/categories-list/categories-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BaseComponent } from '../../../core/base/common-base';
 import { NgxSpinnerService } from 'ngx-spinner';
-import { Router } from '@angular/router';
 import { CategoryService } from '../../../core/services/api-service/category.service';
 import { Category } from '../../../core/models/category.model';
 import { takeUntil } from 'rxjs';
@@ -10,6 +9,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-categories-list',
@@ -27,12 +27,14 @@ import { CommonModule } from '@angular/common';
 })
 export class CategoriesListComponent extends BaseComponent implements OnInit {
   categoriesList: Category[] = [];
-  showInputField = false;
+  showInputField: boolean = false;
   newCategoryName!: Category | null;
+  isEditMode: boolean = false;
+  categoryToEdit: Category | null = null;
   constructor(
     private categoryService: CategoryService,
     private spinner: NgxSpinnerService,
-    private router: Router
+    private toastr: ToastrService
   ) {
     super();
   }
@@ -51,12 +53,18 @@ export class CategoriesListComponent extends BaseComponent implements OnInit {
     // Check if categories are already loaded, if not, fetch from server
     if (this.categoryService.getCurrentCategories().length === 0) {
       this.categoryService.fetchCategories().subscribe();
+      this.spinner.hide();
     }
-    this.spinner.hide();
   }
 
   addNewCategory(category: Category): void {
     this.categoryService.addCategory(category);
+  }
+
+  editCategory(category: Category): void {
+    this.showInputField = true;
+    this.isEditMode = true;
+    this.newCategoryName = category;
   }
 
   removeCategory(category: Category): void {
@@ -64,10 +72,20 @@ export class CategoriesListComponent extends BaseComponent implements OnInit {
   }
 
   submitCategory(): void {
-    if (this.newCategoryName) {
+    const categoryExists = this.categoriesList.some(
+      (existingCategory) => existingCategory === this.newCategoryName
+    );
+    if (this.newCategoryName && !this.isEditMode && !categoryExists) {
       this.addNewCategory(this.newCategoryName);
-      this.newCategoryName = null;
-      this.showInputField = false;
+      this.toastr.success('Category Added Successfully!');
+    } else if (this.newCategoryName && this.isEditMode && !categoryExists) {
+      this.categoryService.updateCategory(this.newCategoryName);
+      this.toastr.success('Category Updated Successfully!');
+    } else {
+      this.toastr.warning('Category Already Exists.');
     }
+    this.newCategoryName = null;
+    this.showInputField = false;
+    this.isEditMode = false;
   }
 }
